07c0228c40fcadf52a0b5bb26741b57e2313be93
Raxesh Shah <raxesh35@gmail.com>
2022-02-03 23:23:17 +0530

    Added switcher for dark/light mode for the body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = '#042743';
+    }
+  }
   return (
     <>
-      <Navbar title="Textutils" />
+      <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <Textform heading="Enter the text for analyze below"/>
+        <Textform mode={mode} heading="Enter the text for analyze below"/>
       </div>
     </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -32,19 +32,19 @@ export default function Textform(props) {
     }
     return (
         <>
-            <div className='container'>
-                <h2>{props.heading}</h2>
+            <div className='container' style={{color: props.mode === 'dark'?'white':'#042743'}}>
+                <h2 >{props.heading}</h2>
                 <b className='text-success'>{copy}</b>
                 <div className="mb-3">
                     <label htmlFor="mybox" className="form-label"></label>
-                    <textarea className="form-control" id="mybox" onChange={handleChangeInput} value={text} rows="8"></textarea>
+                    <textarea className="form-control" id="mybox" onChange={handleChangeInput} value={text} rows="8" style={{backgroundColor: props.mode === 'light'?'white':'grey', color: props.mode === 'dark'?'white':'#042743'}}></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleltuInput}>Convert to uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleutlInput}>Convert to lowercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleCopyToClipboard}>Copy to clipboard</button>
                 <button className="btn btn-primary mx-1" onClick={handleClearnput}>Clear content</button>
             </div>
-            <div className='container'>
+            <div className='container' style={{color: props.mode === 'dark'?'white':'#042743'}} >
                 <h2>Your enter text summery</h2>
                 <p>Text have total <b>{text.split(" ").length}</b> words and <b>{text.length}</b> characters</p>
                 <p>It will take <b>{0.008 * text.split(" ").length}  minutes </b> to read content </p>
